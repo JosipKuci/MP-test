1b2b9e4445de81b55734f301ac14ab7306e27147
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-06-03 09:54:03 +0200

    Test with mpremote

---
+++ PirSensor/PirSensor/Examples/PirSensor-Native-example.py
@@ -0,0 +1,21 @@
+# FILE:   PirSensor-Native-example.py
+# AUTHOR: Josip Šimun Kuči @ Soldered
+# BRIEF:  An example showing how to detect movement
+#         via the PIR sensor over the DOUT pin
+# WORKS WITH: PIR Movement sensor board: www.solde.red/333083
+# LAST UPDATED: 2025-05-23
+from PirSensor import PIRSensor
+from machine import Pin
+import time
+
+# Initialize the PIR sensor
+pir = PIRSensor(pin=Pin(13))
+pir.begin()
+
+while 1:
+    # If movement is detected, the SOUT or DOUT pin will go into a HIGH state
+    if pir.get_state():
+        # Inform the user that motion was detected
+        print("Motion detected!")
+        # Pause for 1 second
+        time.sleep(1)
---
+++ PirSensor/PirSensor/Examples/PirSensor-Qwiic-example.py
@@ -0,0 +1,24 @@
+# FILE: PirSensor-Qwiic-example.py
+# AUTHOR: Josip Šimun Kuči @ Soldered
+# BRIEF: An example showing how to detect movement
+#        via the PIR sensor over the Qwiic connector
+# WORKS WITH: PIR Movement sensor board with easyC: www.solde.red/333179
+# LAST UPDATED: 2025-05-23
+from PirSensor import PIRSensor
+import time
+
+# Initialize the sensor at a specific address
+pir = PIRSensor(address=0x30)  # By default the address is 0x30
+pir.begin()  # Initialize I2C communication with the PIR sensor
+
+# Infinite loop
+while 1:
+    # Check if the pir sensor is responding via I2C
+    if pir.available():
+        # If movement is detected, inform user
+        if pir.get_state():
+            # Set how long the delay from the sensor will be before alerting of movement
+            pir.set_delay(1)
+            print("Motion detected!")
+            # Pause for 1 second
+            time.sleep(1)
